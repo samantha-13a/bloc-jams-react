06f4d6ae06f2ca41e8274e6ffd78078f60cfa840
Samantha Acampora <faeriegarden@Samanthas-MacBook-Pro.local>
2018-11-17 15:04:14 -0500

    Trying to fix all my crap

---
+++ src/components/Album.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import React, { Component } from 'react';
+
+class Album extends Component {
+    render() {
+        return (
+            <section className="album">
+                {this.props.match.params.slug} Album will go here
+            </section>
+        );
+    }
+}
+
+export default Albums;
